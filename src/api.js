d09d3cdc2580ca00db129bda00fc73525347680b
emanuelcardozo <leuname.01@gmail.com>
2023-08-02 12:42:07 -0300

    Adding training steps

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://192.168.0.16:5001'
+const API_URL = 'http://10.1.1.120:5001'
 
 function uploadDocumentToAPI({ trainingMode, text, metadata }) {
   // return fetch(`${API_URL}/${trainingMode}/documents`, {
